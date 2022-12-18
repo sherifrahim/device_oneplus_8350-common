4030c928d904fea6146b14eacb787da33a33b793
Sherif Rahim <28657313+sherifrahim@users.noreply.github.com>
2022-12-18 14:51:09 +0530

    riced

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_lahaina.mk
+    $(LOCAL_DIR)/lineage_lahaina.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_lahaina-user \
-    derp_lahaina-userdebug \
-    derp_lahaina-eng
+    lineage_lahaina-user \
+    lineage_lahaina-userdebug \
+    lineage_lahaina-eng
